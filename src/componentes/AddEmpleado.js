56a3261fa948b9e17fec30f5cafa2e8b87d4b08c
Gustavo R. Galindo [T360] <ggalindo@internal.corpacsteel.com>
2022-11-28 12:01:34 -0500

    Formulario Equipos

--- src/componentes/AddEmpleado.js
+++ src/componentes/AddEmpleado.js
@@ -11,50 +11,45 @@ export const AddEmpleado =() => {
                 <h3 className="text-center mt-5">REGISTRAR EMPLEADO </h3>
                 <div className="card-body ">
                 
-                <form id="addForm" action="#" method="" className="needs-validation" novalidate autocomplete="off">
-                    <div className="form-row">                   
-                        <div className="form-group col-lg-4">
-                            <label htmlFor="inputDate">Nombre</label>
-                            <input type="text" className="form-control" id="inputDate" name="date" placeholder='Nombre del Empleado' required />
+                    <form id="addForm" action="#" method="" className="needs-validation" novalidate autocomplete="off">
+                        <div className="form-row">                   
+                            <div className="form-group col-lg-4">
+                                <label htmlFor="inputDate">Nombre</label>
+                                <input type="text" className="form-control" id="inputDate" name="date" placeholder='Nombre del Empleado' required />
+                                
+                            </div>                    
+                        </div>
+                        
+                        <div className="form-row">
+
+                            <div className="form-group col-lg-6">
+                                <label htmlFor="inputName">Cargo</label>
+                                <input type="text" className="form-control" id="Cargo" name="cargo" placeholder="Cargo del Empleado" required />
+                            </div>                        
+                        </div>
+
+                        <div className="form-row">
+
+                            <div className="form-group col-lg-6">
+                                <label htmlFor="inputName">Cedula</label>
+                                <input type="text" className="form-control" id="Cedula" name="cedula" placeholder="Cedula del Empleado" required />
+                            </div>                           
                             
                         </div>
-                
-                        <hr />   
-                    </div>
-                    
-                    <div className="form-row">
-
-                        <div className="form-group col-lg-6">
-                            <label htmlFor="inputName">Cargo</label>
-                            <input type="text" className="form-control" id="Cargo" name="cargo" placeholder="Cargo del Empleado" required />
-                        </div>   
-                       
                         
-                    </div>
-
-                    <div className="form-row">
-
-                        <div className="form-group col-lg-6">
-                            <label htmlFor="inputName">Cedula</label>
-                            <input type="text" className="form-control" id="Cedula" name="cedula" placeholder="Cedula del Empleado" required />
-                        </div>   
-                       
+                        <hr />
                         
-                    </div>
-                    
-                    <hr />
-                    
-                        
-                    <div className="form-row">
-                    <button className="btn btn-primary bg-instaya btn-block col-lg-4 mt-5" type="submit">Registrar</button>
-                    
-                    <Link to="/Home" relative="/" className="btn btn-primary bg-dark btn-block col-lg-4 mt-5 ml-5"> 
-                        
-                            Cancelar
-                        
-                    </Link>
-                    </div>
-                </form>                
+                            
+                        <div className="form-row">
+                            <button className="btn btn-primary bg-instaya btn-block col-lg-4 mt-5" type="submit">Registrar</button>
+                            
+                            <Link to="/Home" relative="/" className="btn btn-primary bg-dark btn-block col-lg-4 mt-5 ml-5"> 
+                                
+                                    Cancelar
+                                
+                            </Link>
+                        </div>
+                    </form>                
                 </div>
             </div>
             
--- src/index.js
+++ src/index.js
@@ -9,6 +9,8 @@ import { Equipos} from './componentes/Equipos';
 import { Hoja} from './componentes/Hoja';
 import { Empleado } from './componentes/Empleado';
 import { AddEmpleado } from './componentes/AddEmpleado';
+import { AddEquipo } from './componentes/AddEquipo';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -21,6 +23,7 @@ root.render(
         <Route path='/Hoja' element={<Hoja />}/>
         <Route path='/Empleado' element={<Empleado />}/>
         <Route path='/AddEmpleado' element={<AddEmpleado />}/>
+        <Route path='/AddEquipo' element={<AddEquipo />}/>
       </Routes>
     </Router>
    
